0224147a536ed6f7266df0a0dbd7b43597ffe3ab
DiegoCastroT <diegodct6@gmail.com>
2024-11-11 20:56:17 +0100

    Added background

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * Board has 9 cards.
+ * @returns {Element}
+ * @constructor
+ */
+function Board() {
+
+    return (
+
+        //background.
+        <div className="background">
+
+        </div>
+    )
+}
+
+export default Board;
