5926c37997b03f24dd70725b01b406544203df6f
Diego <diegodct6@gmail.com>
2024-11-18 12:43:53 +0100

    Fixed bug: removed duplicated headers.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Board from "./components/Board";
-import Headers from"./components/Header";
 
 function App() {
 
     return (
         <div className="App">
-            <Headers />
             <Board />
         </div>
     );
